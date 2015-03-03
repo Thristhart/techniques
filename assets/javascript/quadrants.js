d2e604241e219e513388e09e624a830abcd8a8b8
Tom Shea <tom@shea.at>
2015-03-03 14:56:52 -0500

    Quadrant render/switching

--- assets/javascript/quadrants.js
+++ assets/javascript/quadrants.js
@@ -3,6 +3,24 @@ function demo(container) {
   var context = canvas.getContext("2d");
   container.appendChild(canvas);
   
+  var diagMode = document.createElement("input");
+  diagMode.type = "radio";
+  diagMode.name = "mode";
+  diagMode.checked = true;
+  var rectMode = document.createElement("input");
+  rectMode.type = "radio";
+  rectMode.name = "mode";
+  
+  container.appendChild(diagMode);
+  container.appendChild(rectMode);
+  
+  diagMode.addEventListener("changed", function(event) {
+    if(diagMode.selected)
+      mode = "diagonal";
+    else
+      mode = "rectangle";
+  });
+  
   canvas.width = 200;
   canvas.height = 200;
   
@@ -33,6 +51,57 @@ function demo(container) {
       context.moveTo(0, 0);
       context.lineTo(canvas.width / 2, canvas.height / 2);
       context.lineTo(canvas.width, 0);
+      context.closePath();
+      context.stroke();
+      // left
+      context.beginPath();
+      context.moveTo(0, 0);
+      context.lineTo(canvas.width / 2, canvas.height / 2);
+      context.lineTo(0, canvas.height);
+      context.closePath();
+      context.stroke();
+      // bottom
+      context.beginPath();
+      context.moveTo(0, canvas.height);
+      context.lineTo(canvas.width / 2, canvas.height / 2);
+      context.lineTo(canvas.width, canvas.height);
+      context.closePath();
+      context.stroke();
+      // right
+      context.beginPath();
+      context.moveTo(canvas.width, 0);
+      context.lineTo(canvas.width / 2, canvas.height / 2);
+      context.lineTo(canvas.width, canvas.height);
+      context.closePath();
+      context.stroke();
+    }
+    if(mode === "rectangle") {
+      // topleft
+      context.beginPath();
+      context.moveTo(0, 0);
+      context.lineTo(canvas.width / 2, canvas.height / 2);
+      context.lineTo(canvas.width / 2, 0);
+      context.closePath();
+      context.stroke();
+      // bottomleft
+      context.beginPath();
+      context.moveTo(0, canvas.height / 2);
+      context.lineTo(canvas.width / 2, canvas.height / 2);
+      context.lineTo(canvas.width / 2, canvas.height);
+      context.closePath();
+      context.stroke();
+      // bottomright
+      context.beginPath();
+      context.moveTo(canvas.width, canvas.height / 2);
+      context.lineTo(canvas.width / 2, canvas.height / 2);
+      context.lineTo(canvas.width / 2, canvas.height);
+      context.closePath();
+      context.stroke();
+      // topright
+      context.beginPath();
+      context.moveTo(canvas.width / 2, 0);
+      context.lineTo(canvas.width / 2, canvas.height / 2);
+      context.lineTo(canvas.width, canvas.height / 2);
       context.closePath();
       context.stroke();
     }
